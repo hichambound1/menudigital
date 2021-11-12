c3a9f8b2eeb8b7c1973c9d15cdcd34873becc39c
hichambound1 <h.boundouq@gmail.com>
2021-11-12 17:29:08 +0100

    add auth api

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name_en' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+
+        $role= Role::where('name','user')->first();
+        if($role== NULL){
+            $role= Role::create([
+                'name'=>'user'
+            ]);
+        }
+        $user= User::create([
+            'name_en' =>$request->name_en,
+            'email' => $request->email,
+            'statu' => 0,
+            'role_id'  => $role->id,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,31 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
 use App\Http\Controllers\Api\v1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+// public routes
 
-Route::get('/category', [CategoryController::class, 'index']);
+ // public auth routes
+ Route::post('/register', [AuthController::class, 'register']);
+ Route::post('/login', [AuthController::class, 'login']);
+
+// protected route
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/category', [CategoryController::class, 'index']);
+
+
+
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
+
+
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
