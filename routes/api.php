3342aeda6d845c899c54c16f00b2cf1c37b36f21
Othmane_Rhazlani <oghazlani0@gmail.com>
2021-11-28 16:48:38 +0100

    some change

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Api\v1\CategoryController;
 use App\Http\Controllers\Api\v1\DishController;
 use Illuminate\Support\Facades\Route;
 
-
 // public routes
 
  // public auth routes
@@ -20,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 
     Route::get('/mydishes/{id}', [DishController::class, 'mydishes']);
     Route::post('/dish/store', [DishController::class, 'store']);
-
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
